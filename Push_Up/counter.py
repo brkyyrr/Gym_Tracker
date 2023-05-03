51c16d755e2e63a300e5c20edd315a0b517a2970
brkyyrr <berkayyurur@gmail.com>
2023-05-04 01:50:22 +0300

    Düzeltmeler yapılmıştır.

--- Push_Up/counter.py
+++ Push_Up/counter.py
@@ -95,12 +95,6 @@ with mp_pose.Pose( min_detection_confidence=0.5,
 
             left_arm_angle = int(calculate_angle(shoulder, elbow, wrist))
             right_arm_angle = int(calculate_angle(shoulder_r, elbow_r, wrist_r))
-            left_leg_angle = int(calculate_angle(left_hip, left_knee, left_ankle))
-            right_leg_angle = int(calculate_angle(right_hip, right_knee, right_ankle))
-            left_arm_length = np.linalg.norm(np.array(shoulder) - np.array(elbow))
-
-            mid_point_x = (int(left_hip[0] * image_width )+ int(right_hip[0] * image_width))/2
-            mid_point_y = (int(left_hip[1] * image_height)+ int(right_hip[1] * image_height))/2
 
             landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].visibility = 0
             landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].visibility = 0
@@ -129,9 +123,6 @@ with mp_pose.Pose( min_detection_confidence=0.5,
             cv2.line(image,(int(shoulder_r[0]* image_width),int(shoulder_r[1]* image_height)),(int(elbow_r[0]* image_width),int(elbow_r[1]* image_height)),(255,255,255),3)
 
             cv2.line(image,(int(neck_point_x),int(neck_point_y)),(int(based_mid_x),int(based_mid_y)),(255,255,255),3,cv2.LINE_4)
-            cv2.line(image,(int(based_mid_x),int(based_mid_y)),(int(left_hip[0] * image_width ),(int(left_hip[1] * image_height))),(255,255,255),3,cv2.LINE_8)
-            cv2.line(image,(int(based_mid_x),int(based_mid_y)),(int(right_hip[0] * image_width ),(int(right_hip[1] * image_height))),(255,255,255),3,cv2.LINE_8)
-
             cv2.circle(image,(int(neck_point_x),int(neck_point_y)),4,(255,255,255),5)
 
             cv2.circle(image,(int(shoulder[0]* image_width),int(shoulder[1]* image_height)),4,(255,255,255),3)
